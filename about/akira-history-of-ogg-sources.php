71ac094aeea0c92f13511414deb6ba566021f268
quozl <quozl@us.netrek.org>
2007-05-23 21:24:18 +0000

    adding a misc history file

---
+++ about/akira-history-of-ogg-sources.php
@@ -0,0 +1,94 @@
+<?php
+// This is the WEB PAGE TEMPLATE FILE
+//
+// Rules:
+// 1) Correct the path of the "require 'nhp.php'" if necessary
+// 2) Uncomment the Sidebars that are required for this page (at least 1)
+// 3) The Header and Footer must remain untouched
+// 4) add your file to the $siteparentlist array in nhp.php.
+// 5) Add your page content
+
+require '../set_my_root.php';
+require '../nhp.php';
+
+doNhpHeader();
+
+echo getSidebarHints();
+// echo getSidebarNews();
+// echo getSidebarColofon();
+
+echo "</div>\n\n<div id=\"content\">";
+
+?>
+
+
+<p>These are the two versions of KMB/Akira/Sun Tzu's history of the term Ogg.
+
+<p><h2>Earlier Account</h2>
+
+An earlier account by the same author existed in the Netrek Nexus glossary, 
+and has been removed to here. <br>It seems a bit more specific about names 
+and dates. </center> 
+<br><p> 
+Back in, I believe, November/December of 1990 I use to play netrek  
+much more than now. At the time, I was probably only Flt Captain Sun Tzu,  
+maybe even just a Captain. A group of us, which included  
+Jay Hui (TheSlug), Byron Sinor (Krang), and Steve Russel (Feakhelek) 
+were playing netrek with Terence. The three of us were Feds,  
+Terence was Orion.</p> 
+ 
+<p>It was a full game, and Terence had come in as  
+Og &lt;== (beginning to get how it happened :&gt; ).  
+At this time, suiciding people with kills *NEVER* happened.  
+Dogfighting rained supreme, and cloaking was used only for planet taking,  
+that is until Terence decided to teach us CMU boys alesson. </p> 
+ 
+<p>Steve had accumulated a 3, 4 kill ship and had taken Spica (or El Nath,  
+I don't remember) and Terence chased him and tried to mutual with him.  
+Steve killed him, but was left at ~90% internal.  
+As he wiggled towards Fed. space he watched the galactic map as  
+Og reappeared and began to move rapidly in his direction.  
+I will never forget how his voice shattered the calm of the cluster  
+with yells of</p> 
+<p>Its OGGGGGGGG. HELP! HELP!!!!!!!!! Its OOOOOOGGGGGGGGGGGGGGGG!!!. </p> 
+<p>His final words were something to the effect of AAAAAAAAAArrrrrrrgggggghhhhhh, Ive been Ogged!!.</p> 
+Jay and I immediately picked up on the effectiveness of suiciding people, and had Terence show us how to do it better. <br>That is how Ogging started getting popular, and how the name spread through Netrek History. [Kevin Bernatz]  
+ 
+ 
+
+
+
+<p><h2>2006 Wikipedia Account</h2>
+
+<br><p> While the concept of the suicide attack may have occurred in 
+other games, such as Xpilot, Xtrek or Empire, the term "Ogg" was first 
+used in either the Baker or Wean Hall computer labs at Carnegie Mellon 
+University in the 1990-1991 school year (I do not remember which exact 
+lab or whether it was before or after Xmas break that year). 
+ 
+<p>Myself, Jay Hui and Steve Rancunus (sp?)
+were playing netrek in the early hours  
+of the morning. Steve was a Fed (F# something) and was scumming against  
+a lone Orion player. The server god at the time (Terence Chang,  
+who brought netrek to CMU from Berkeley the year before), was  
+watching the game and logged in as one of the non-player  
+robot slots (the game is 8x8, so the player slots are 0-9,a,b,c,d,e, and f).  
+ 
+<p> 
+Since he joined the Orions and was the first "robot" in the game,  
+he was "Og". Terence was one of the best players in the game and  
+proceeded to terrorize Steve, such that at one point in the middle  
+of the quiet computer lab, Steve burst out  
+with "Help!! It's 'Oh' 'Gee'!". Followed quickly by additional  
+screams of "Help" and the first uttering  
+of "It's Ogggg! It's Ogggg! Helllppp!!".  
+
+<p>Given the hilariousness  
+of the situation, the term followed Steve and quickly  
+was picked up among other netrek players. 
+
+<?
+echo "</div>\n";
+echo getFooter();
+?>
+
