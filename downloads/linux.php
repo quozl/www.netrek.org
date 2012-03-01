5e949a6e5d729e4333f6b3bb9a326f54788c1635
quozl <quozl@us.netrek.org>
2012-03-01 00:15:40 +0000

    remove obsolete linux entries

--- downloads/linux.php
+++ downloads/linux.php
@@ -23,21 +23,16 @@ echo "</div>\n\n<div id=\"content\">";
 
 <h2>The Netrek COW client</h2><br>
 <p>
-There are a number of Netrek clients available for Linux. COW is the most actively supported and developed. If you are interested in clients besides COW, we've got <a href="./linux-not-cow.php">a page full of them</a>. See the following table for instructions for installing COW on your system. Once installed, run the client by typing "netrek-client-cow" in a terminal window. </p>
+There are a number of Netrek clients available for Linux. COW and Gytha are the most actively supported and developed. If you are interested in clients besides COW, we've got <a href="./linux-not-cow.php">a page full of them</a>. See the following table for instructions for installing COW on your system. Once installed, run the client by typing "netrek-client-cow" in a terminal window. </p>
 
 <table>
 <tr bgcolor="#0066FF"><th>Your distribution<th>Instructions
 
-<tr bgcolor="#333333"><td>Debian Squeeze <td><a href="./linux-cow.php#apt">Use an apt package tool, instructions at (1)</a>
-<tr><td>Debian Sid     <td><a href="./linux-cow.php#apt">Use an apt package tool, instructions at (1)</a>
-<tr bgcolor="#333333"><td>Ubuntu Jaunty Jackalope &nbsp;&nbsp;<td><a href="./linux-cow.php#apt">Use an apt package tool, instructions at (1)</a>
-
-<tr><td>Debian Etch  <td><a href="./linux-cow.php#cow327">Follow the COW 3.2.7 .deb instructions at (2)</a>
-<tr bgcolor="#333333"><td>Debian Lenny <td><a href="./linux-cow.php#cow327">Follow the COW 3.2.7 .deb instructions at (2)</a>
-<tr><td>Ubuntu Hardy Heron<td><a href="./linux-cow.php#cow327">Follow the COW 3.2.7 .deb instructions at (2)</a>
-<tr bgcolor="#333333"><td>Ubuntu Intrepid Ibex<td><a href="./linux-cow.php#cow327">Follow the COW 3.2.7 .deb instructions at (2)</a>
-<tr><td>Not listed above, <br>give me a binary tarball<td><a href="./linux-cow.php#cowtarball">Try directions at (3)</a>
-<tr bgcolor="#333333"><td>I want to build from source code<td><a href="./linux-cow.php#sourcebuild">Follow the source build directions at (4)</a>
+<tr bgcolor="#333333"><td>Debian<td><a href="./linux-cow.php#apt">Install the package from Debian</a>
+<tr><td>Ubuntu<td><a href="./linux-cow.php#apt">Install the package from Ubuntu</a>
+<tr bgcolor="#333333"><td>Mandriva<td>Install the package from Mandriva
+<tr>Other<td><a href="./linux-cow.php#cowtarball">A binary tarball</a>
+<tr bgcolor="#333333"><td>Other<td><a href="./linux-cow.php#sourcebuild">Build from source code</a>
 <tr><td>
 </table>
 
